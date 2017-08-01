b463957d8d5ca9572b88b2218592fd330cd57c11
ijat <me@ijat.my>
2017-08-01 14:31:24 +0800

    Add project files.

---
+++ cs-cloud-shell-folder/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs_cloud_shell_folder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
